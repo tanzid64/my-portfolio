4b3d16719f315940d530025146dc1b9f3ae8ad7e
tanzid64 <tanzid@inbox.ru>
2024-03-26 14:16:00 +0600

    Skill text updated

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -35,7 +35,7 @@ export default function Skills() {
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4 dark:text-white">{title}</p>
+              <p className="mt-4  dark:text-white text-[12px] md:text-md">{title}</p>
             </div>
           ))}
         </div>
